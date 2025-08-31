2793de84fd759472de7ffd03912eaf1bbec3c521
Maksim Shkrabak <shkrabakmaksim@gmail.com>
2025-08-30 19:16:44 -0700

    Implement font weight toggle based on radio button selection

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,11 @@
 function changeFont() {
-  document.getElementById("textArea").style.fontSize = "2em";
+  document.getElementById("textArea").style.fontSize = "3em";
+}
+
+function changeWeight() {
+  if (document.getElementById("fancyButton").checked) {
+    document.getElementById("textArea").style.fontWeight = "bold";
+  } if (document.getElementById("boringButton").checked) {
+    document.getElementById("textArea").style.fontWeight = "normal";
+  }
 }
