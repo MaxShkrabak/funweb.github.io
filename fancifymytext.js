6a62712a6b0a70938345e25cc097c490ce5e5b05
Maksim Shkrabak <shkrabakmaksim@gmail.com>
2025-08-30 20:46:14 -0700

    Implement new button 'Moo' that uppercases text and adds a suffix

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,10 +3,30 @@ function changeFont() {
 }
 
 function changeWeight() {
-  
   if (document.getElementById("fancyButton").checked) {
     document.getElementById("textArea").className = "fancy";
   } if (document.getElementById("boringButton").checked) {
     document.getElementById("textArea").className = "boring";
   }
+}
+
+function moo() {
+  let text = document.getElementById("textArea");
+
+  let currentText = text.value.toUpperCase();
+  let parts = currentText.split(".");
+
+  for (let i = 0; i < parts.length; i++) {
+    let trimmedPart = parts[i].trim();
+    
+    // Add '-Moo' if there's at least one letter
+    if (/[A-Z]/.test(trimmedPart)) {
+      parts[i] = trimmedPart + "-Moo"; 
+    } else {
+      // Do nothing
+    }
+  }
+  
+  text.value = parts.join(".");
+
 }
